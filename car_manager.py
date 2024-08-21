6cadd251ddaf53d9a2c2f24a683f885fb7ee31f9
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2024-08-21 19:25:02 +0530

    Created Car manager class that generates cars

---
+++ car_manager.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+import random
+from constants import (
+    CAR_WIDTH_FACTOR, CAR_HEIGHT_FACTOR,
+    CAR_COLORS, CAR_SHAPE,
+    CAR_HEADING, CAR_X_STARTING_POSITION,
+    CAR_UPPER_Y_LIMIT, CAR_LOWER_Y_LIMIT,
+    CAR_WIDTH,
+    CAR_STARTING_MOVE_SPEED, CAR_MOVE_SPEED_INCREMENT
+)
+
+
+class Car(Turtle):
+    def __init__(self, y_position):
+        super().__init__()
+        self.shape(CAR_SHAPE)
+        self.penup()
+        self.shapesize(stretch_wid=CAR_WIDTH_FACTOR, stretch_len=CAR_HEIGHT_FACTOR)
+        self.color(random.choice(CAR_COLORS))
+        self.setx(CAR_X_STARTING_POSITION)
+        self.sety(y_position)
+        self.setheading(CAR_HEADING)
+
+    def move(self, speed):
+        self.setx(self.xcor() - speed)
+
+
+class CarManager:
+
+    def __init__(self):
+        self.cars = []
+        self.speed = CAR_STARTING_MOVE_SPEED
+
+    def generate_cars(self):
+        random_y_position = random.randint(CAR_LOWER_Y_LIMIT, CAR_UPPER_Y_LIMIT)
+        print(CAR_LOWER_Y_LIMIT)
+        print(CAR_UPPER_Y_LIMIT)
+        new_car = Car(random_y_position)
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.move(self.speed)
+
+    def update_speed(self):
+        self.speed += CAR_MOVE_SPEED_INCREMENT
+
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from constants import (
     TURTLE_UP_KEY, TURTLE_Y_COR_BOUNDARY
 )
 from scoreboard import ScoreBoard
+from car_manager import CarManager
 
 screen = Screen()
 screen.bgcolor(SCREEN_BACKGROUND_COLOR)
@@ -20,16 +21,26 @@ screen.listen()
 player = Player()
 screen.onkey(key=TURTLE_UP_KEY, fun=player.move)
 
+car_manager = CarManager()
+
 scoreboard = ScoreBoard()
 
 is_game_on = True
-
+loop_count = 0
 while is_game_on:
     screen.update()
     time.sleep(SCREEN_UPDATE_TIME)
 
+    if loop_count % 6 == 0:
+        car_manager.generate_cars()
+
+    car_manager.move_cars()
+
     if player.ycor() >= TURTLE_Y_COR_BOUNDARY:
         scoreboard.increase_level()
         player.reset_player()
+        car_manager.update_speed()
+
+    loop_count += 1
 
 screen.exitonclick()
