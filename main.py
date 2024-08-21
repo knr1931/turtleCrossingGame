9b3b68cc387f550f1cdcae206d01714f0f566256
Nitesh Reddy <7451291+knr1931@users.noreply.github.com>
2024-08-21 15:15:24 +0530

    Created Level functionality in SCoreboard

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import (
     SCREEN_BACKGROUND_COLOR,
     TURTLE_UP_KEY, TURTLE_Y_COR_BOUNDARY
 )
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.bgcolor(SCREEN_BACKGROUND_COLOR)
@@ -19,16 +20,16 @@ screen.listen()
 player = Player()
 screen.onkey(key=TURTLE_UP_KEY, fun=player.move)
 
+scoreboard = ScoreBoard()
+
 is_game_on = True
 
 while is_game_on:
     screen.update()
     time.sleep(SCREEN_UPDATE_TIME)
 
-    print(player.ycor())
-
     if player.ycor() >= TURTLE_Y_COR_BOUNDARY:
+        scoreboard.increase_level()
         player.reset_player()
-
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+from constants import (
+    SCORE_BOARD_POSITION, SCOREBOARD_ALIGNMENT, SCOREBOARD_FONT, SCOREBOARD_COLOR
+)
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.color(SCOREBOARD_COLOR)
+        self.hideturtle()
+        self.penup()
+        self.goto(SCORE_BOARD_POSITION)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg=f"Level: {self.level}", move=False, align=SCOREBOARD_ALIGNMENT, font=SCOREBOARD_FONT)
+
+    def game_over(self):
+        self.goto(x=0, y=0)
+        self.write(arg="Game Over", move=False, align=SCOREBOARD_ALIGNMENT, font=SCOREBOARD_FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.update_scoreboard()
